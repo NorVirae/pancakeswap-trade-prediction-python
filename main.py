b74fa3ce5d429b9209e3cd704d2029e9adef149b
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-02-19 07:20:31 +0100

    remove sensitive data

--- main.py
+++ main.py
@@ -19,11 +19,35 @@ mainAbiFile = json.loads(data)
 rpc_mainnet = "https://data-seed-prebsc-1-s1.binance.org:8545/"
 provider = Web3(Web3.HTTPProvider(rpc_mainnet))
 
-walletAddress = "0xBC5B9D64284fA4B723d085Acbe543C01721E75B4"
-privateKey = "7ad1155ef711e1956a1cefc98a3546479e359499116692e9206dab1b3babfacc"
+walletAddress = ""
+privateKey = ""
 # contract
 contract = provider.eth.contract(address=addressContract, abi=mainAbiFile)
-print(addressContract, mainAbiFile)
 # get currentEpoch
 current_epoch = contract.functions.currentEpoch().call()
-print(current_epoch)
+
+# Take a look back to some 3k epoch
+lookback = 3
+start_epoch = current_epoch - lookback
+
+rounds = ["epoch",
+  "start_timestamp",
+  "lock_timestam",
+  "close_timestamp",
+  "lock_price",
+  "close_price",
+  "lock_oracleId",
+  "close_oracleId",
+  "total_amount",
+  "bull_amount",
+  "bear_amount",
+  "reward_baseCalAmount",
+  "reward_amount",
+  "oracle_called",]
+
+count = 1
+for e in range(1, lookback):
+    time.sleep(1)
+    start_epoch += 1
+    count += 1
+    print(start_epoch, count)
